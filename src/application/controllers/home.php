1d09b64d08f3078414f37994e2f4b0146f9f743d
Justin Lewis <jtl1728@rit.edu>
2009-11-04 23:52:04 -0500

    Home page now only shows tagged files as homepage.
    Also added file list link to header.php

--- src/application/controllers/home.php
+++ src/application/controllers/home.php
@@ -4,17 +4,28 @@ class Home extends Controller {
         parent::Controller();
         $this->load->model('db_files');
         $this->load->library('parser');
+        $this->load->config('display_settings');
     }
 	
     function index(){
         $data['page_title'] = 'Home';
-        $data['filelist'] = $this->db_files->getFileList();
+        
+        $data['filelist'] = $this->db_files->getFilesByTag($this->config->item('homepage_tag'));
 
         $this->load->view('header', $data);
         $this->parser->parse( 'file_list', $data );
         $this->load->view('footer');
     }
 
+    function files(){
+        $data['page_title'] = 'File List';
+        $data['filelist'] = $this->db_files->getFileList();
+
+        $this->load->view('header', $data);
+        $this->parser->parse( 'file_list', $data );
+        $this->load->view('footer');
+    }
+    
     function tag( $tag_id=null ){
         if( $tag_id != null ){
             $data['page_title'] = "Home &gt; Files taged under $tag_id";
--- src/application/views/header.php
+++ src/application/views/header.php
@@ -8,5 +8,6 @@
 <div class="navigation">
 	<?php echo anchor("/home","Home"); ?> |
 	<?php echo anchor("/home/tag","Tag List"); ?> |
+	<?php echo anchor("/home/files","File List"); ?> |
 	<?php echo anchor("/upload","Upload File"); ?>
 </div>

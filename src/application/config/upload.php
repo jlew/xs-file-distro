f11e71e6d23c7963c8580060c51c0571ed5f1822
Justin Lewis <jtl1728@rit.edu>
2009-11-19 14:48:31 -0500

    Added some default types and image size limitations.

--- src/application/config/upload.php
+++ src/application/config/upload.php
@@ -2,6 +2,8 @@
 $config['upload_path'] = './uploads/';
 
 //This is the path to the uploads dir from your site root.
-$config['public_path'] = '/uploads/';
-$config['allowed_types'] = 'pdf|gif|jpg|png';
+$config['public_path'] = 'uploads/';
+$config['allowed_types'] = 'pdf|gif|jpg|png|xo|doc|txt';
 $config['remove_spaces'] = true;
+$config['max_width'] = '1024';
+$config['max_height'] = '768';

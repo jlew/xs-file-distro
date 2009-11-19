6ba4b75f0c292c612eb46737b113279c00c08324
Justin Lewis <jtl1728@rit.edu>
2009-11-19 15:24:45 -0500

    Added xo file to mimes.php config.  Fixed config bug found in upload.

--- src/application/config/upload.php
+++ src/application/config/upload.php
@@ -3,7 +3,10 @@ $config['upload_path'] = './uploads/';
 
 //This is the path to the uploads dir from your site root.
 $config['public_path'] = 'uploads/';
-$config['allowed_types'] = 'pdf|gif|jpg|png|xo|doc|txt';
+
+//Bug in code igniter, must put images at end of list.
+//http://codeigniter.com/bug_tracker/bug/6780/
+$config['allowed_types'] = 'pdf|xo|zip|doc|txt|gif|jpg|png';
 $config['remove_spaces'] = true;
 $config['max_width'] = '1024';
 $config['max_height'] = '768';
